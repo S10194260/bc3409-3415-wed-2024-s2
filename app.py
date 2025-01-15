d77c34c7bed78ff34da4d5dcf74ad57082b23024
Shannon <98508966+S10194260@users.noreply.github.com>
2025-01-15 21:32:38 +0000

    1

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import Flask
 from flask import render_template, request 
+
+app = Flask(__name__) # assigning this app to me
+
+@app.route("/") # @ -> to run the 1st line
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__": # to double confirm that the application belongs to me
+    app.run()
+    
